17bf9735b5295fffc1f4b9d0245970980bc8b785
OpenSauce <lawleycobhc@gmail.com>
2020-09-01 21:23:50 +0100

    exports

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var fs = require("fs");
-
 PDFParser = require("pdf2json");
 
-let pdfParser = new PDFParser();
+exports.character = {};
+exports.loadCharacter = (file) => {
+      let pdfParser = new PDFParser();
+      pdfParser.loadPDF(file);
+}
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+let pdfParser = new PDFParser();
+pdfParser.loadPDF("HuYaoBang.pdf");
+
+pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
     pdfParser.on("pdfParser_dataReady", pdfData => {
           var sheetValues = pdfParser.getAllFieldsTypes();
+          fs.writeFile('content.txt', JSON.stringify(pdfData), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
           for(var element in sheetValues) {
                 console.log(sheetValues[element].id + ": ");
-                var obj = sheetValues[element].value.replace(/\r?\n|\r/g, '');
+                var obj = sheetValues[element].value.replace(/\r/g, '\n');
                 console.log(obj);
           }
     });
- 
-pdfParser.loadPDF("HuYaoBang.pdf");
